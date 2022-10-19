1b29f7fbecdd8887c5d31487baa926f94e968ef0
nadeemb53 <nadeemb53@gmail.com>
2022-10-19 18:41:03 +0530

    new test failing

--- contracts/EcdsaSecp256k1Ops.sol
+++ contracts/EcdsaSecp256k1Ops.sol
@@ -28,7 +28,66 @@ contract EcdsaSecp256k1Ops {
 
     function sign(bytes32 message, uint256 privKey) external pure {}
 
-    function verify() external pure {}
+    function verify(
+        string calldata message,
+        bytes calldata sig,
+        address addr
+    ) external pure returns (bool) {
+        bytes32 msgHex = ethMessageHash(message);
+        return recover(msgHex, sig) == addr;
+    }
+
+    function recover(bytes32 hash, bytes memory sig)
+        internal
+        pure
+        returns (address)
+    {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        // Check the signature length
+        if (sig.length != 65) {
+            return (address(0));
+        }
+
+        // Divide the signature in r, s and v variables
+        // ecrecover takes the signature parameters, and the only way to get them
+        // currently is to use assembly.
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+        if (v < 27) {
+            v += 27;
+        }
+
+        // If the version is correct return the signer address
+        if (v != 27 && v != 28) {
+            return (address(0));
+        } else {
+            // solium-disable-next-line arg-overflow
+            return ecrecover(hash, v, r, s);
+        }
+    }
+
+    function ethMessageHash(string memory message)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return
+            keccak256(
+                abi.encodePacked(
+                    "\x19Ethereum Signed Message:\n32",
+                    keccak256(abi.encodePacked(message))
+                )
+            );
+    }
 
     function batchVerify() external pure {}
 }
--- test/ops.test.js
+++ test/ops.test.js
@@ -1,17 +1,25 @@
 const ops = artifacts.require("EcdsaSecp256k1Ops");
 const jsrsasign = require("jsrsasign");
 
-contract("ops", () => {
+contract("ops", (accounts) => {
+  const alg = "SHA256withECDSA";
+  const msg = "bridging is tough";
+  const curve = "secp256k1";
   const charlen = 256;
-  const ec = new jsrsasign.KJUR.crypto.ECDSA({ curve: "secp256k1" });
+
+  const ec = new jsrsasign.KJUR.crypto.ECDSA({ curve: curve });
+  const keypair = ec.generateKeyPairHex();
+  const sig = new jsrsasign.KJUR.crypto.Signature({ alg: alg });
+  sig.init({ d: keypair.ecprvhex, curve: curve });
+  sig.updateString(msg);
+  const sigHex = sig.sign();
 
   beforeEach(async () => {
     opsContract = await ops.deployed();
   });
 
-  it("Derive public key from private key", async () => {
+  it("derives public key from private key", async () => {
     // generate a new key pair
-    const keypair = ec.generateKeyPairHex();
     // pass the new private key to the contract
     var result = await opsContract.derivePubKey("0x".concat(keypair.ecprvhex));
     var hX = result[0].toString(16).slice(-charlen);
@@ -21,5 +29,10 @@ contract("ops", () => {
     console.log(keypair.ecpubhex);
     // test if both public key hex are identical
     assert.equal(hPub, keypair.ecpubhex);
+  });
+
+  it("verifies ECDSA signature", async () => {
+    var result = await opsContract.verify(msg, sigHex, accounts[0]);
+    console.log(result);
   });
 });
