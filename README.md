ce0bfa889d320466784c6d6356f106e5a28deba5
nadeemb53 <nadeemb53@gmail.com>
2022-10-25 17:40:32 +0530

    working test

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ npx hardhat compile
 npx hardhat node
 npx hardhat run scripts/deploy.ts
 npx hardhat test
+forge test
 ```
 ## [Gas report](gas-report.txt)
 
@@ -17,5 +18,9 @@ Max signature count is 153, beyond which the transaction gas limit exceeds block
 ### EcdsaOps
 
 > Contract includes encoding and hashing of message string, looping through each signature and recovering each address from the message hash and signature using `ECDSA.recover` function. Finally, an event is emitted with the number of correct matches.
+
+### BenchEcrecover
+
+> Contract for benchmarking ecrecover precompile using foundry's forge tests in `test/forge-test`.
 
 
--- test/forge-test/test-ops.t.sol
+++ test/forge-test/test-ops.t.sol
@@ -1,17 +1,69 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "../../Contracts/EcdsaOps.sol";
+import "../../Contracts/BenchEcrecover.sol";
 import "../../lib/forge-std/src/Test.sol";
 
 contract EcdsaOpsTest is Test {
-    uint256 a;
+    BenchEcrecover public ops;
+    uint256 NUMBER = 8000;
 
     function setUp() public virtual {
-        a = 10;
+        ops = new BenchEcrecover();
     }
 
-    function testExample() public {
-        assertEq(a, 10);
+    function test() public {
+        verify();
+    }
+
+    function verify() public {
+        bytes32 message = ethMessageHash("TEST");
+
+        bytes
+            memory sig = hex"bceab59162da5e511fb9c37fda207d443d05e438e5c843c57b2d5628580ce9216ffa0335834d8bb63d86fb42a8dd4d18f41bc3a301546e2c47aa1041c3a1823701";
+        address addr = 0x999471bB43B9C9789050386F90C1Ad63DCa89106;
+
+        recover(message, sig, addr);
+    }
+
+    function recover(
+        bytes32 hash,
+        bytes memory sig,
+        address addr
+    ) internal {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+        if (v < 27) {
+            v += 27;
+        }
+
+        ops.benchmarkEcrecover(hash, v, r, s, addr, NUMBER);
+    }
+
+    /**
+     * @dev prefix a bytes32 value with "\x19Ethereum Signed Message:" and hash the result
+     */
+    function ethMessageHash(string memory message)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return
+            keccak256(
+                abi.encodePacked(
+                    "\x19Ethereum Signed Message:\n32",
+                    keccak256(abi.encodePacked(message))
+                )
+            );
     }
 }
